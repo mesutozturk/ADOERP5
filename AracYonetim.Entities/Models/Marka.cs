20bdd9a639218743c30d681032bf31b54e562a0a
Mesut OZTURK <mesutoztrk@msn.com>
2018-01-18 17:45:55 +0300

    biseswar

--- AracYonetim.Entities/Models/Marka.cs
+++ AracYonetim.Entities/Models/Marka.cs
@@ -17,25 +17,14 @@ namespace AracYonetim.Entities.Models
         public string Ulke { get; set; }
         public string Kurucusu { get; set; }
         public byte[] Logo { get; set; } //varbinary
-        
-
-
-
-
-        public string Soner { get; set; }
-
         public virtual List<Arac> Araclar { get; set; } = new List<Arac>();
         public override string ToString() => $"{this.MarkaAdi}";
 
+        public string Soner { get; set; }
         public string Recep { get; set; }
-
         public string Alican61 { get; set; }
-
         public string Mesut { get; set; }
         public string MuratD { get; set; }
         public string MuratBircan { get; set; }
-
-
-
     }
 }
