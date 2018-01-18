1c0c79d1b88dfaa092dafade17e57dab05bc5e47
Wissen <Wissen@WISSEN-PC>
2018-01-18 17:30:06 +0200

    Ömer

--- AracYonetim.Entities/Models/Marka.cs
+++ AracYonetim.Entities/Models/Marka.cs
@@ -12,15 +12,26 @@ namespace AracYonetim.Entities.Models
         [Required]
         [StringLength(50)]
         public string MarkaAdi { get; set; }
+
         [Range(1850, 2100, ErrorMessage = "Kurulus Yılı 1850-2100 arasında olabilir")]
+
+
         public int KurulusYili { get; set; }
+
         public string Ulke { get; set; }
+
         public string Kurucusu { get; set; }
+
+
         public byte[] Logo { get; set; } //varbinary
 
         public virtual List<Arac> Araclar { get; set; } = new List<Arac>();
+
         public override string ToString() => $"{this.MarkaAdi}";
 
+
         public string Omer { get; set; }
+
+
     }
 }
