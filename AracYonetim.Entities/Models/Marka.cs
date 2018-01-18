669bfdd3aa1a924eb9a4b3dc7de0f01799706edc
AlicanYildirim <yildirimalican61@gmail.com>
2018-01-18 16:03:06 +0200

    Alican

--- AracYonetim.Entities/Models/Marka.cs
+++ AracYonetim.Entities/Models/Marka.cs
@@ -20,5 +20,7 @@ namespace AracYonetim.Entities.Models
 
         public virtual List<Arac> Araclar { get; set; } = new List<Arac>();
         public override string ToString() => $"{this.MarkaAdi}";
+
+        public string Alican61 { get; set; }
     }
 }
